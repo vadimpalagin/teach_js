8d9943a9e48aba0d446cc350e50548306f826779
Vadim <example@gmail.com>
2022-08-25 11:01:50 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+"use strict"
+
+// const answer = prompt("Вам есть 18?", "");
+// console.log(answer);
+
+// alert('Hello');
+
+const isChecked = false,
+      isClose = false;
+console.log(isChecked || !isClose);
